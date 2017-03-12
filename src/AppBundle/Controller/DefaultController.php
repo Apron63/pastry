90a1690b979624ff816435f185c6f3537e705a74
Apron63 <apron63@yandex.ru>
2017-03-12 18:48:03 +0400

    Добавлена маршрутизация

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,32 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction(Request $request)
+    {
+        return $this->render('default/about.html.twig.');
+    }
+
+    /**
+     * @Route("/order", name="order")
+     */
+    public function orderAction(Request $request)
+    {
+        return $this->render('default/order.html.twig.');
+    }
+
+    /**
+     * @Route("/club", name="club")
+     */
+    public function clubAction(Request $request)
+    {
+        return $this->render('default/club.html.twig.');
     }
 }
